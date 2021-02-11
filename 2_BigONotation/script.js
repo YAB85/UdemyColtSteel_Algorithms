5c4c7019e9b68da9ea509f0666e4e87b0b8528c8
Yaroslav <boyaal24@gmail.com>
2021-02-11 22:33:06 +0200

    add section about Big 0

--- 2_BigONotation/script.js
+++ 2_BigONotation/script.js
@@ -6,10 +6,30 @@ function addUpTo(n) {
   return total;
 }
 
-console.log(addUpTo(8));
+console.log(addUpTo(1000000));
 
 function addUpToSecond(n) {
   return (n * (n + 1)) / 2;
 }
 
-console.log(addUpToSecond(6));
+console.log(addUpToSecond(1000000));
+
+function countUpAndDown(n) {
+  console.log("Going up!");
+  for (let i = 0; i < n; i++) {
+    console.log(i);
+  }
+  console.log("At the top!\nGoing down...");
+  for (let j = n - 1; j >= 0; j--) {
+    console.log(j);
+  }
+  console.log("Back down.Bye!");
+}
+
+function printAllPairs(n) {
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      console.log(i, j);
+    }
+  }
+}
